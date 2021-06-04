99fe3328939c5e8c25745f15a398457595464f6f
Aung Ko Man <aungkoman@gmail.com>
2021-06-04 20:32:06 +0630

    https upgrade in 2d

--- 2d/assets/js/registration.js
+++ 2d/assets/js/registration.js
@@ -1,45 +1,42 @@
+$(document).on('click', '#register', function() {
+    if ($('#createpassword').val() == $('#confirmpassword').val()) {
+        var username = $('#createusername').val();
+        var password = $('#confirmpassword').val();
+        var phonenumber = $('#phonenumber').val();
+        var balance = $('#createbalance').val();
+        var role = "user";
 
-$(document).on('click', '#register', function () {
-    if($('#createpassword').val()==$('#confirmpassword').val()){
-    var username = $('#createusername').val();
-    var password = $('#confirmpassword').val();
-    var phonenumber = $('#phonenumber').val();
-    var balance = $('#createbalance').val();
-    var role="user";
+        var settings = {
+            "url": "https://track-gps-backend.herokuapp.com/api/v1/users",
+            "method": "POST",
+            "timeout": 0,
+            "headers": {
+                "Content-Type": "application/x-www-form-urlencoded"
+            },
+            "data": {
+                "name": username,
+                "password": password,
+                "role": role,
+                "phonenumber": phonenumber,
+                "balance": balance
+            }
+        };
 
-    var settings = {
-      "url": "http://track-gps-backend.herokuapp.com/api/v1/users",
-      "method": "POST",
-      "timeout": 0,
-      "headers": {
-        "Content-Type": "application/x-www-form-urlencoded"
-      },
-      "data": {
-        "name": username,
-        "password": password,
-        "role": role,
-        "phonenumber": phonenumber,
-        "balance": balance
-      }
-    };
-  
-    $.ajax(settings).done(function (response) {
-      if (response.status == true) {  
-        $('#createusername').val("");
-        $('#createpassword').val("")
-        $('#confirmpassword').val("");
-        $('#phonenumber').val("");
-        $('#createbalance').val("");    
-        alert("User Registration Successfully!");
-      }
-      else {
-        alert("User Registration Fail!");
-      }
-  
-    });
-}
-else{
-    alert("Password Do not Match!");
-}
-  
-  });
+        $.ajax(settings).done(function(response) {
+            if (response.status == true) {
+                $('#createusername').val("");
+                $('#createpassword').val("")
+                $('#confirmpassword').val("");
+                $('#phonenumber').val("");
+                $('#createbalance').val("");
+                alert("User Registration Successfully!");
+            } else {
+                alert("User Registration Fail!");
+            }
+
+        });
+    } else {
+        alert("Password Do not Match!");
+    }
+
+});
